f45b30bfe43d697d401f794171521b4450da597e
Daniel Fonseca <fonsecafelipedaniel@gmail.com>
2024-12-28 18:26:23 -0500

    solución 127

--- 217.py
+++ 217.py
@@ -1,7 +1,15 @@
+"""""
 lista = [1,1,1,3,3,4,3,2,4,2]
 boli= False
 for i in range(0, len(lista)):
     for j in range(i+1, len(lista)):
         if lista[i] == lista[j]:
             boli=True
+print(boli)
+"""
+boli= False
+lista = [1,1,1,3,3,4,3,2,4,2]
+seteo = set(lista)
+if len(seteo) < len(lista):
+    boli= True
 print(boli)
